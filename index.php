92a13e5b5a4b82d14729b69fb8904504fa3bd714
Grishakova Irina <irina.grishh@gmail.com>
2022-07-11 11:08:07 +0300

    table work, be not buttom

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-  require_once 'configDB.php';
+  require 'configDB.php';
+  require 'tasks.php';
   ?>
 
 <!DOCTYPE html>
@@ -17,36 +18,56 @@
 </head>
 <body>
 	<div class="container">
-<h1>Список задач</h1>
+
 <form action="/planner/add.php" method="post">
 	<input type="text" name="task" id="task_input" placeholder="Добавить задачу" class="form_control">
 	<button type='submit' name="send_task" class="btn btn-success">Отправить задачу</button>
 </form>
-  <?php
-  //require_once 'configDB.php';
 
-  echo '<div class="column">';
-
-  
-  $query = $pdo->query('SELECT * FROM `task_process` order by `id` DESC');
-  while($row = $query->fetch(PDO::FETCH_OBJ)) {
-
-  	echo '<table class="table table-sm">';
-foreach($row as $v=>$k) {
-        echo '<th scope="row">'.$v.'</th><tbody><td class="table-success">'.$k.'</tbody></td>';
-        echo '<form action="/delete.php?id='.$k->id.'" method="get">
-
-	<button type="delete" name="delete" class="btn btn-success">Удалить</button>
-</form>';
-    }
-echo '</table>';
-  	
-  	echo '<div class="row justify-content-center"><b>'.$row->task.'</b><a href="/delete.php?id='.$row->id.'"><button>Удалить</button></a></div>';
-      }
-  echo '</div>';
-
-  ?>
  
   </div>
+  <div class="container" >
+  <h1>Список задач</h1>
+
+  <table class="table table-success table-hover">
+  		<tr>
+   	<th>задача</th>
+   	<th>время</th>
+   	<th>действия</th>
+   	</tr>
+  <?php
+
+
+ //zip for name columns
+
+   foreach($data as $row) {
+   	$id = $row['id'];
+   	$task = $row['task'];
+   	$time = $row['time'];
+   	
+   	?>
+
+   	<tr>
+   	<td><?php echo $task ?></td>
+   	</td>
+   	<td><?php echo $time?></td>
+
+   	</tr>
+   	
+<?php
+   	 }
+   	?>
+
+
+   	
+    
+
+	
+
+     
+
+ 
+</table>
+</div>
 </body>
 </html>
