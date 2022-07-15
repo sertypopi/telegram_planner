1de981730c073142d4b0e2fdc5addb2f074af8cb
Grishakova Irina <irina.grishh@gmail.com>
2022-07-16 00:50:47 +0300

    work add

--- add.php
+++ add.php
@@ -1,19 +1,33 @@
+<?php 
+require 'configDB.php';
+?>
+
 <?php
+var_dump($_POST);
     $task = $_POST['task'];
     if($task == '') {
-	    echo 'Не указана задача';
-	    exit();
+      echo 'Не указана задача';
+      exit();
     }
 
+    $status = 'в очереди';
 
-  require 'configDB.php';
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+
+
+  
  
-    $sql = 'INSERT INTO task_process(task) VALUES(:task)';
+    $sql = "INSERT INTO task_process(task, status) VALUES(:task, :status)";
     $query = $pdo->prepare($sql);
-    $query->execute(['task'=> $task]);
+    print_r($query);
+    $query->execute(['task'=> $task, 'status'=> $status]);
+
+
 
 
     header('Location: /planner/');
+     
 
 
 
